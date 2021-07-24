6c3713b6a6c0f20e1632325bd08471018d4b2bd6
Omar Monteon <monteon.omar@gmail.com>
2021-07-24 08:29:56 -0500

    refactor: Update App to be a function component.
    refactor: Remove toggleTheme from state.
    Pass directly since its only used in Nav.
    
    refactor: Update Nav to get the theme using the context hook.
    Also getting toggleTheme as prop instead.

--- app/index.js
+++ app/index.js
@@ -1,57 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from './contexts/theme'
-import Loading from './components/Loading'
-import Nav from './components/Nav'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from './contexts/theme';
+import Loading from './components/Loading';
+import Nav from './components/Nav';
 
-const Posts = React.lazy(() => import('./components/Posts'))
-const Post = React.lazy(() => import('./components/Post'))
-const User = React.lazy(() => import('./components/User'))
+const Posts = React.lazy(() => import('./components/Posts'));
+const Post = React.lazy(() => import('./components/Post'));
+const User = React.lazy(() => import('./components/User'));
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
+function App() {
+  const [theme, setTheme] = useState('light');
+  function toggleTheme() {
+    setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
   }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className="container">
+            <Nav toggleTheme={toggleTheme} />
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route
-                    exact
-                    path='/'
-                    render={() => <Posts type='top' />}
-                  />
-                  <Route
-                    path='/new'
-                    render={() => <Posts type='new' />}
-                  />
-                  <Route path='/post' component={Post} />
-                  <Route path='/user' component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" render={() => <Posts type="top" />} />
+                <Route path="/new" render={() => <Posts type="new" />} />
+                <Route path="/post" component={Post} />
+                <Route path="/user" component={User} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
           </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
+        </div>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'));
